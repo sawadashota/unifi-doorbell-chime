42bef8e679d07cba794c740da06f1bb725898e49
sawadashota <shota@sslife.tech>
2021-04-30 17:01:23 +0900

    fix: Refactor listener (#21)

--- listener/listener.go
+++ listener/listener.go
@@ -6,8 +6,8 @@ import (
 	"time"
 
 	"github.com/cenkalti/backoff/v4"
-
 	"github.com/pkg/browser"
+	"github.com/pkg/errors"
 	"github.com/sawadashota/unifi-doorbell-chime/x/unifi"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/xerrors"
@@ -45,11 +45,9 @@ func (l *Listener) poll(ctx context.Context) error {
 
 	for _, d := range ds {
 		if d.DoesRung(l.state) {
-			if err := l.openURL(&d); err != nil {
-				return xerrors.Errorf("failed to open browser: %w", err)
+			if err := l.onRung(&d); err != nil {
+				return errors.WithStack(err)
 			}
-
-			l.logger.Infof("%s (%s) is rung!\n", d.Name, d.Mac)
 		}
 	}
 
@@ -62,32 +60,9 @@ const pollingInterval = 1 * time.Second
 func (l *Listener) Start(ctx context.Context) error {
 	defer l.logger.Info("Bye!")
 
-	bc := backoff.NewExponentialBackOff()
-	bc.MaxElapsedTime = time.Minute * 5
-	bc.Reset()
-
-	var doorbells unifi.Doorbells
-	err := backoff.Retry(func() error {
-		if err := l.r.UnifiClient().Authenticate(); err != nil {
-			l.logger.Error(err)
-			return xerrors.Errorf("failed to start listener: %w", err)
-		}
-
-		var err error
-		doorbells, err = l.r.UnifiClient().GetDoorbells(ctx)
-		if err != nil {
-			l.logger.Error(err)
-			return xerrors.Errorf("failed to start listener: %w", err)
-		}
-		return nil
-	}, bc)
-	if err != nil {
+	if err := l.ping(ctx); err != nil {
 		l.logger.Error(err)
-		return xerrors.Errorf("failed to start listener: %w", err)
-	}
-
-	for _, d := range doorbells {
-		l.logger.Infof("activate %s ID: %s\n", d.Name, d.ID)
+		return errors.WithStack(err)
 	}
 
 	ticker := time.NewTicker(pollingInterval)
@@ -106,9 +81,38 @@ func (l *Listener) Start(ctx context.Context) error {
 		}
 	}
 }
+func (l *Listener) ping(ctx context.Context) error {
+	bc := backoff.NewExponentialBackOff()
+	bc.MaxElapsedTime = time.Minute * 5
+	bc.Reset()
 
-func (l *Listener) openURL(doorbell *unifi.Doorbell) error {
-	return browser.OpenURL(
+	return backoff.Retry(func() error {
+		if err := l.r.UnifiClient().Authenticate(); err != nil {
+			l.logger.Error(err)
+			return xerrors.Errorf("failed to authenticate: %w", err)
+		}
+
+		doorbells, err := l.r.UnifiClient().GetDoorbells(ctx)
+		if err != nil {
+			l.logger.Error(err)
+			return xerrors.Errorf("failed to start listener: %w", err)
+		}
+
+		for _, d := range doorbells {
+			l.logger.Infof("activate %s ID: %s\n", d.Name, d.ID)
+		}
+		return nil
+	}, bc)
+}
+
+func (l *Listener) onRung(doorbell *unifi.Doorbell) error {
+	err := browser.OpenURL(
 		fmt.Sprintf("http://127.0.0.1:%d/ringing/%s", l.c.WebPort(), doorbell.ID),
 	)
+	if err != nil {
+		return xerrors.Errorf("failed to open browser: %w", err)
+	}
+
+	l.logger.Infof("%s (%s) is rung!\n", doorbell.Name, doorbell.Mac)
+	return nil
 }
